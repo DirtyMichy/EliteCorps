18c46da6c708b792c855f3e1cfcb1a48bfa3ca7e
DirtyMichy <michy1138@web.de>
2020-03-11 00:44:34 +0100

    Revert "Revert "Remaster""
    This reverts commit 652ec072d40615d0d0f2fb7807aae750786fa5c9.

--- Assets/Standard Assets/Effects/ImageEffects/Scripts/MotionBlur.cs
+++ Assets/Standard Assets/Effects/ImageEffects/Scripts/MotionBlur.cs
@@ -21,11 +21,13 @@ namespace UnityStandardAssets.ImageEffects
 
         override protected void Start()
         {
+            /*
             if (!SystemInfo.supportsRenderTextures)
             {
                 enabled = false;
                 return;
             }
+            */
             base.Start();
         }
 
