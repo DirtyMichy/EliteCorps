43b99dcc15500afb30a019e08a8002dc04c5af12
DirtyMichy <Michy1138@web.de>
2020-11-03 21:39:57 +0100

    CleanUp

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -23,12 +23,23 @@ public class Manager : MonoBehaviour
     private Vector2[] playerDpad;
     private GamepadInput.GamePad.Index[] gamePadIndex = { GamePad.Index.One, GamePad.Index.Two, GamePad.Index.Three, GamePad.Index.Four };
 
+    [Header("✠ - ✠ - ✠ - ✠ - ✠ - ✠ - ✠ Enemy Spawn ✠ - ✠ - ✠ - ✠ - ✠ - ✠ - ✠")]
+
+    public int seconds = 0;
+    public GameObject[] bosses;
+    public GameObject[] enemies;
+    public GameObject[] enemyBlobs;
+    public GameObject[] objectives;
+    public GameObject objectiveToKill;
+    private Vector2 min, max;
+    private float campaingObjectiveSpawn = 30;
+    private float timeBetweenSpawns = 20;
+    private int maxRngForWaves = 2;
+
     [Header("✠ - ✠ - ✠ - ✠ - ✠ - ✠ - ✠ Objective ✠ - ✠ - ✠ - ✠ - ✠ - ✠ - ✠")]
 
-    private bool bossSpawned = false;
     public bool objectiveComplete = true;
     public int requiredObjectivesToKill = 2;
-    private int MAXSECONDS = 128;
     public int MAXPLAYERS = 4;
     public int requiredKillsToWin = 64;
     public int currentMainMenuSelection = 0;        //Used for iteration with arrow keys oder dpad inside the main menu
@@ -40,6 +51,8 @@ public class Manager : MonoBehaviour
     public GameObject[] PlayableCharacters;
     public GameObject[] missionObjects;
     public GameObject[] player;
+    private bool bossSpawned = false;
+    private int MAXSECONDS = 128;
 
     public enum missionObjectives
     {
@@ -97,7 +110,7 @@ public class Manager : MonoBehaviour
     [Header("✠ - ✠ - ✠ - ✠ - ✠ - ✠ - ✠ Gameplay ✠ - ✠ - ✠ - ✠ - ✠ - ✠ - ✠")]
 
     public GameObject explosion;
-    public GameObject spawnManager;
+    //public GameObject spawnManager;
     public GameObject Background;
     public GameObject escortPlane;
     public GameObject escortShip;
@@ -128,6 +141,9 @@ public class Manager : MonoBehaviour
 
     void Awake()
     {
+        min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
         fader.SetActive(true);
 
         if (current == null)
@@ -397,7 +413,200 @@ public class Manager : MonoBehaviour
         GetComponent<AudioSource>().Play();
         StartCoroutine("VolumeOn");
 
-        spawnManager.GetComponent<EnemyManager>().StartSpawnCoroutines();
+        StartSpawnCoroutines();
+    }
+
+    public void StartSpawnCoroutines()
+    {
+        StartCoroutine("Spawn");
+        //global seetings
+        maxRngForWaves = 2;
+        seconds = 0;
+        timeBetweenSpawns = 20 / Manager.current.playerCount;
+
+        //Level specific settings
+        switch (Manager.current.currentMissionSelected)
+        {
+            case 0:
+                campaingObjectiveSpawn = 15;
+                objectiveToKill = objectives[Random.Range(0, 4)];
+                break;
+            case 2:
+                campaingObjectiveSpawn = 30;
+                objectiveToKill = objectives[0];
+                break;
+            case 3:
+                timeBetweenSpawns = 4 / Manager.current.playerCount;
+                break;
+            case 8:
+                campaingObjectiveSpawn = 20;
+                objectiveToKill = objectives[1];
+                break;
+            default:
+                break;
+        }
+    }
+    public void StopSpawnCoroutines()
+    {
+        StopCoroutine("Spawn");
+    }
+
+    void Waves()
+    {
+        if (seconds % 30 == 0)
+            maxRngForWaves = Mathf.Clamp((int)(maxRngForWaves + 1), 0, enemies.Length);
+        int rng = Random.Range(0 + (maxRngForWaves / 4), maxRngForWaves); //-1
+
+        Instantiate(enemies[rng], enemies[rng].transform.position = new Vector2(Random.Range(min.x + 1, max.x - 1), 8f), enemies[rng].transform.rotation);
+    }
+
+    public void spawnBoss(int bossNumber)
+    {
+        Instantiate(bosses[bossNumber]);
+    }
+
+    //Spawns enemies and counts seconds
+    public IEnumerator Spawn()
+    {
+        Debug.Log("StartCoroutineSpawn");
+        while (Manager.current.currentMenu == Manager.activeMenu.None)
+        {
+            Debug.Log("StartCoroutineSpawn");
+            Debug.Log("Spawning");
+
+            if (Manager.current.gameMode == Manager.selectedGameMode.campaign && seconds % timeBetweenSpawns == 0)
+            {
+                missionSpawns();
+            }
+
+            if (Manager.current.gameMode == Manager.selectedGameMode.survive)
+            {
+                Waves();
+            }
+
+            yield return new WaitForSeconds(1f);
+
+            seconds++;
+        }
+    }
+
+    void missionSpawns()
+    {
+        min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        float spawnPosition = 0;
+        float screenWidth = Mathf.Abs(min.x) + max.x;
+
+        if (Manager.current.missionMode == Manager.missionObjectives.killObjective)
+        {
+            //Spawning additional Objectives, if you missed the objective another one should spawn faster
+            if (Manager.current.objectiveKills < Manager.current.requiredObjectivesToKill && (seconds % campaingObjectiveSpawn == 0) && (seconds > 60))
+            {
+                int episode = Manager.current.currentMissionSelected;
+                Instantiate(objectiveToKill);
+                campaingObjectiveSpawn = 10;
+            }
+        }
+
+        //############################################################ EPISODE I ############################################################
+
+        if (Manager.current.currentMissionSelected < 5)
+        {
+            //Destroy 4 bunkers
+            if (Manager.current.currentMissionSelected == 0)
+            {
+                Instantiate(enemyBlobs[4]);
+            }
+
+            //Escort boat
+            if (Manager.current.currentMissionSelected == 1)
+            {
+                Instantiate(enemyBlobs[Random.Range(0, 2)], enemyBlobs[1].transform.position, enemyBlobs[1].transform.rotation);
+            }
+
+            //Destroy drillingPit
+            if (Manager.current.currentMissionSelected == 2)
+            {
+                Instantiate(enemyBlobs[Random.Range(0, 3)], enemyBlobs[1].transform.position, enemyBlobs[1].transform.rotation);
+            }
+
+            //Destroy all ships
+            if (Manager.current.currentMissionSelected == 3)
+            {
+                GameObject[] enemiesAlive = GameObject.FindGameObjectsWithTag("Enemy");
+                spawnPosition = Random.Range(min.x + 1, max.x - 1);
+                int rng = Random.Range(0, 8);
+
+                if (enemiesAlive.Length < Manager.current.requiredKillsToWin - Manager.current.kills)
+                    if (rng == 0)
+                        Instantiate(enemies[4], enemies[4].transform.position = new Vector2(spawnPosition, 8f), enemies[4].transform.rotation);
+                    else
+                        Instantiate(enemies[1], enemies[1].transform.position = new Vector2(spawnPosition, 8f), enemies[1].transform.rotation);
+
+            }
+
+            //Boss
+            if (Manager.current.currentMissionSelected == 4)
+            {
+                int rng = Random.Range(0, 2); //-1
+
+                if (rng == 0)
+                {
+                    Instantiate(enemyBlobs[Random.Range(0, 2)], enemyBlobs[1].transform.position, enemyBlobs[1].transform.rotation);
+                }
+            }
+
+            //############################################################ EPISODE II ############################################################
+
+            if (Manager.current.currentMissionSelected > 4 && Manager.current.currentMissionSelected < 10)
+            {
+                //Escort 2 Planes
+                if (Manager.current.currentMissionSelected == 5)
+                {
+                    Instantiate(enemyBlobs[0], enemyBlobs[0].transform.position, enemyBlobs[0].transform.rotation);
+                }
+
+                //Survive 
+                if (Manager.current.currentMissionSelected == 6)
+                {
+                    Instantiate(enemyBlobs[Random.Range(0, 2)], enemyBlobs[1].transform.position, enemyBlobs[1].transform.rotation);
+                }
+
+                //Kill all
+                if (Manager.current.currentMissionSelected == 7)
+                {
+                    Instantiate(enemyBlobs[Random.Range(0, 3)], enemyBlobs[1].transform.position, enemyBlobs[1].transform.rotation);
+                }
+
+                //Destroy 3 fortresses
+                if (Manager.current.currentMissionSelected == 8)
+                {
+                    GameObject[] enemiesAlive = GameObject.FindGameObjectsWithTag("Enemy");
+                    spawnPosition = Random.Range(min.x + 1, max.x - 1);
+                    int rng = Random.Range(0, 8);
+
+                    if (enemiesAlive.Length < Manager.current.requiredKillsToWin - Manager.current.kills)
+                        if (rng == 0)
+                            Instantiate(enemies[4], enemies[4].transform.position = new Vector2(spawnPosition, 8f), enemies[4].transform.rotation);
+                        else
+                            Instantiate(enemies[1], enemies[1].transform.position = new Vector2(spawnPosition, 8f), enemies[1].transform.rotation);
+
+                }
+
+                //Boss
+                if (Manager.current.currentMissionSelected == 9)
+                {
+                    int rng = Random.Range(0, 2); //-1
+
+                    if (rng == 0)
+                    {
+                        Instantiate(enemyBlobs[Random.Range(0, 2)], enemyBlobs[1].transform.position, enemyBlobs[1].transform.rotation);
+                    }
+                }
+
+            }
+        }
     }
 
     //Show the missionObjective at start
@@ -513,7 +722,7 @@ public class Manager : MonoBehaviour
         GetComponent<AudioSource>().Play();
 
         objectiveComplete = false;                                          //Otherwise the winscreeen would load directly
-        spawnManager.GetComponent<EnemyManager>().StopSpawnCoroutines();    //Stop spawning props and units
+        StopSpawnCoroutines();    //Stop spawning props and units
 
         yield return new WaitForSeconds(5f);
 
@@ -531,7 +740,7 @@ public class Manager : MonoBehaviour
             GetComponent<AudioSource>().Play();
             GetComponent<AudioSource>().volume = 100f;
 
-            spawnManager.GetComponent<EnemyManager>().spawnBoss(Missions[currentMissionSelected].GetComponent<Mission>().thisEpisode);
+            spawnBoss(Missions[currentMissionSelected].GetComponent<Mission>().thisEpisode);
         }
 
         // EXTRA: Controls change during BOSS01 after 10 seconds
@@ -589,7 +798,7 @@ public class Manager : MonoBehaviour
 
     public IEnumerator ShowHighScore()
     {
-        if(gameMode == selectedGameMode.survive)
+        if (gameMode == selectedGameMode.survive)
             objectiveComplete = true;
 
         gameOver.SetActive(!objectiveComplete);
@@ -649,7 +858,7 @@ public class Manager : MonoBehaviour
             GetComponent<AudioSource>().Play();
         }
 
-        spawnManager.GetComponent<EnemyManager>().StopSpawnCoroutines();//Stop spawning props and units
+        StopSpawnCoroutines();//Stop spawning props and units
 
         StopCoroutine("CountDown");
 
@@ -667,7 +876,7 @@ public class Manager : MonoBehaviour
         int highestPlayer = 0;
 
         float pos = 256, posY = 0.64f;
-        
+
         for (int j = 0; j < playerCount; j++)
         {
             //highScoreGUIText[j].transform.localPosition = new Vector3((0 - pos * (playerCount - 1)) + j * (pos * 2), posY, 0f);
@@ -849,23 +1058,23 @@ public class Manager : MonoBehaviour
 
                     float attributeValue;
                     //Debug.Log(Mathf.Abs(-1.5f));
-                    
-                    attributeValue = PlayableCharacters[Mathf.Abs(chosenCharacterIndex[i])].GetComponent<Player>().maxHP/10f;
+
+                    attributeValue = PlayableCharacters[Mathf.Abs(chosenCharacterIndex[i])].GetComponent<Player>().maxHP / 10f;
                     PlayerChosenChar[i].transform.GetChild(3).transform.localScale = new Vector3(attributeValue, PlayerChosenChar[i].transform.GetChild(3).transform.localScale.y, PlayerChosenChar[i].transform.GetChild(3).transform.localScale.z);
-                    
+
                     attributeValue = PlayableCharacters[Mathf.Abs(chosenCharacterIndex[i])].GetComponent<Player>().speed / 10f;
                     PlayerChosenChar[i].transform.GetChild(4).transform.localScale = new Vector3(attributeValue, PlayerChosenChar[i].transform.GetChild(3).transform.localScale.y, PlayerChosenChar[i].transform.GetChild(3).transform.localScale.z);
 
-                    int amountOfCannons =0;
+                    int amountOfCannons = 0;
                     for (int l = 0; l < PlayableCharacters[Mathf.Abs(chosenCharacterIndex[i])].GetComponent<Player>().transform.childCount; l++)
                     {
                         if (PlayableCharacters[Mathf.Abs(chosenCharacterIndex[i])].GetComponent<Player>().transform.GetChild(l).tag == "Cannon")
                             amountOfCannons++;
                     }
 
-                    attributeValue = amountOfCannons/1.5f;
+                    attributeValue = amountOfCannons / 1.5f;
                     PlayerChosenChar[i].transform.GetChild(5).transform.localScale = new Vector3(attributeValue, PlayerChosenChar[i].transform.GetChild(3).transform.localScale.y, PlayerChosenChar[i].transform.GetChild(3).transform.localScale.z);
-                    
+
                 }
 
                 //Keyboard for Player 01
@@ -1029,7 +1238,7 @@ public class Manager : MonoBehaviour
     {
         iTween.MoveTo(missionMarker, iTween.Hash("position", Missions[currentMissionSelected].transform.position, "easeType", "linear", "time", .5f));
 
-        if(currentMissionSelected == 21)
+        if (currentMissionSelected == 21)
         {
             chosenEpisode.text = "Episode\t" + "?" + "\nMission\t" + "?";
             missionText.text = "E?M?";
